406999ffbef6e19ab3749c8034883c086f2acc4d
Derick Hooley <dghooley@gmail.com>
2021-10-23 20:31:10 -0500

    restructureNoteRoutes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const notes = require("../../db/db.json");
 const { createNewNote, deleteNote } = require('../../lib/notes')
 
+router.get("/notes", (req, res) => {
+    let saved = notes;
+    res.json(saved);
+});
+
 router.post("/notes", (req, res) => {
     req.body.id = notes.length.toString();
     let note = createNewNote(req.body, notes);
     res.json(note);
-    console.log("DATABASE", db)
+//    console.log("DATABASE", db)
 })
-
-router.get("/notes", (req, res) => {
-    let saved = notes;
-    res.json(notes);
-});
 
 router.delete('/notes/:id', (req, res) => {
     deleteNote(notes, req.params.id);
