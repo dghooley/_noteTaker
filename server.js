dc451957897feec849476eba1e4c0d38630e931d
Derick Hooley <dghooley@gmail.com>
2021-10-23 19:55:13 -0500

    add lib/notes.js

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const apiRoutes = require("././Develop/routes/apiRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
-app.use("/", htmlRoutes)
-app.use("/api", apiRoutes)
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
 
-// app.listen(PORT, ()=> console.log(`Listening on PORT: ${PORT}`))
+/* app.listen(PORT, ()=> {
+    console.log(`Listening on PORT: ${PORT}`)
+});
+*/
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
 });
