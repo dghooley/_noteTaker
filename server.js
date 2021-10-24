5552949826382377f326e33ccd4f52f6656fc3a9
Derick Hooley <dghooley@gmail.com>
2021-10-23 20:28:32 -0500

    restructure/removeDevelop

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const htmlRoutes = require("./Develop/routes/htmlRoutes");
-const apiRoutes = require("./Develop/routes/apiRoutes/noteRoutes");
+const apiRoutes = require("./Develop/routes/apiRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
